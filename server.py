9848d9ac14283cfef1cd07395cc4259fe8a9daaf
Mu-14 <Mukesh.Vinayaga@disys.com>
2023-08-11 16:05:52 +0530

    now saving image works

--- server.py
+++ server.py
@@ -2,39 +2,51 @@ from flask import Flask, request
 from pynput import mouse
 from infi.systray import SysTrayIcon
 from datetime import datetime
-from PIL import ImageGrab,Image
-import requests, os, signal
+from PIL import ImageGrab
+import requests, os
 
 app = Flask(__name__)
 
+sessionId = ''
+session_data = {}
+
 def on_click(x, y, button, pressed):
+    global sessionId
+
     if pressed:
         print(f'Mouse clicked at ({x}, {y}) with {button}')
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S.%fZ')
         click_type = str(button).split('.')[1].capitalize()
 
-        screenshot = ImageGrab.grab()
-        screenshot_filename = f'screenshot_{timestamp}.png'
         current_dir = os.getcwd()
-        screenshot_path = os.path.join(current_dir, screenshot_filename)
-        screenshot.save(screenshot_path)
+        screenshot_filename = f'{timestamp}.png'
+        screenshotFolder = os.path.join(current_dir, 'screenshots')
+        sessionSubFolder = os.path.join(current_dir, 'screenshots', sessionId)
+        if not os.path.exists(screenshotFolder): os.mkdir(screenshotFolder)
+        if not os.path.exists(sessionSubFolder): os.mkdir(sessionSubFolder)
 
+        screenshot_path = os.path.join(current_dir, 'screenshots', sessionId, screenshot_filename)
         session_data["mouseClicks"].append({
             "timeStamp": timestamp,
             "clickType": click_type,
             "xLocation": x,
             "yLocation": y,
-            "screenshot": screenshot_path  # Add the screenshot path to session_data
+            "fileName": screenshot_filename 
         })
+        screenshot = ImageGrab.grab()
+        screenshot.save(screenshot_path)
 
-session_data = {}
 mouse_listener = mouse.Listener(on_click=on_click)
 
 @app.route('/start-session', methods=['GET'])
 def handle_start():
     global session_data
+    global sessionId
+
+    sessionId = request.args.get('id')
     mouse_listener.start()
-    session_data ={
+    session_data = {
+        'sessionId': sessionId,
         'startedAt': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
         'mouseClicks': [],
     } 
@@ -43,11 +55,11 @@ def handle_start():
 @app.route('/end-session', methods=['GET'])
 def handle_end():
     global session_data
-    session_data['sessionId'] = request.args.get('id')
-    session_data['endedAt'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    global sessionId
 
+    session_data['endedAt'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     response = requests.put('https://appili.gives/items', json=session_data)
-    session_data = {}
+    session_data, sessionId = {}, ''
     mouse_listener.stop()
     return '200'
 
